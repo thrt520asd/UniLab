49d5002d3020ab061fac3cf06c51603ecf868f1e
thrt520 <610162816@qq.com>
2019-03-29 09:55:21 +0800

    增加Readme

---
+++ Assets/Tools/SerializeTools.cs
@@ -0,0 +1,83 @@
+﻿// Serialization.cs
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class SerializeTools
+{
+    public static string ListToJson<T>(List<T> l) 
+    {
+        return JsonUtility.ToJson(new Serialization<T>(l));
+    }
+
+    public static List<T> ListFromJson<T>(string str) 
+    {
+        return JsonUtility.FromJson<Serialization<T>>(str).ToList();
+    }
+
+    public static string DicToJson<TKey, TValue>(Dictionary<TKey, TValue> dic)
+    {
+        return JsonUtility.ToJson(new Serialization<TKey, TValue>(dic));
+    }
+
+    public static Dictionary<TKey, TValue> DicFromJson<TKey, TValue>(string str)
+    {
+        return JsonUtility.FromJson<Serialization<TKey, TValue>>(str).ToDictionary();
+    }
+
+}
+
+
+// List<T>
+[Serializable]
+public class Serialization<T>
+{
+    [SerializeField]
+    List<T> target;
+    public List<T> ToList() { return target; }
+
+    public Serialization(List<T> target)
+    {
+        this.target = target;
+    }
+
+    
+
+}
+
+// Dictionary<TKey, TValue>
+[Serializable]
+public class Serialization<TKey, TValue> : ISerializationCallbackReceiver
+{
+    [SerializeField]
+    List<TKey> keys;
+    [SerializeField]
+    List<TValue> values;
+
+    Dictionary<TKey, TValue> target;
+    public Dictionary<TKey, TValue> ToDictionary() { return target; }
+
+    public Serialization(Dictionary<TKey, TValue> target)
+    {
+        this.target = target;
+    }
+
+    public void OnBeforeSerialize()
+    {
+        keys = new List<TKey>(target.Keys);
+        values = new List<TValue>(target.Values);
+    }
+
+    public void OnAfterDeserialize()
+    {
+        var count = Math.Min(keys.Count, values.Count);
+        target = new Dictionary<TKey, TValue>(count);
+        for (var i = 0; i < count; ++i)
+        {
+            target.Add(keys[i], values[i]);
+        }
+    }
+
+    
+}
